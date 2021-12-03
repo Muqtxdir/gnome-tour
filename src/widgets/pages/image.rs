8a92ca04ef4fd13bde02c6469934636d5debed40
Muqtxdir <muqtadir5555@gmail.com>
2021-12-03 17:34:44 +0530

    add debian packaging from salsa.debian.org

---
+++ src/widgets/pages/image.rs
@@ -0,0 +1,66 @@
+use gtk::prelude::*;
+
+pub struct ImagePageWidget {
+    pub widget: gtk::Box,
+}
+
+impl ImagePageWidget {
+    pub fn new(resource_uri: &str, head: String, body: String) -> Self {
+        let widget = gtk::Box::new(gtk::Orientation::Vertical, 0);
+
+        let image_page = Self { widget };
+
+        image_page.init(resource_uri, head, body);
+        image_page
+    }
+
+    fn init(&self, resource_uri: &str, head: String, body: String) {
+        self.widget.set_property_expand(true);
+        self.widget.get_style_context().add_class("page");
+        self.widget.set_halign(gtk::Align::Fill);
+        self.widget.set_valign(gtk::Align::Fill);
+
+        let container = gtk::BoxBuilder::new()
+            .orientation(gtk::Orientation::Vertical)
+            .spacing(12)
+            .halign(gtk::Align::Center)
+            .valign(gtk::Align::Center)
+            .vexpand(true)
+            .margin_bottom(48)
+            .margin_top(12)
+            .margin_start(12)
+            .margin_end(12)
+            .build();
+
+        let image = gtk::Image::from_resource(&resource_uri);
+        image.set_valign(gtk::Align::Start);
+        image.show();
+        container.add(&image);
+
+        let head_label = gtk::LabelBuilder::new()
+            .label(&head)
+            .justify(gtk::Justification::Center)
+            .valign(gtk::Align::Center)
+            .margin_top(36)
+            .build();
+        head_label.get_style_context().add_class("page-title");
+        head_label.show();
+        container.add(&head_label);
+
+        let body_label = gtk::LabelBuilder::new()
+            .label(&body)
+            .lines(2)
+            .wrap(true)
+            .justify(gtk::Justification::Center)
+            .valign(gtk::Align::Center)
+            .margin_top(12)
+            .build();
+        body_label.get_style_context().add_class("page-body");
+        body_label.show();
+        container.add(&body_label);
+
+        container.show();
+        self.widget.add(&container);
+        self.widget.show();
+    }
+}
---
+++ src/widgets/window.rs
@@ -0,0 +1,116 @@
+use crate::utils::i18n_f;
+use gettextrs::gettext;
+use gtk::prelude::*;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use super::pages::{ImagePageWidget, WelcomePageWidget};
+use super::paginator::PaginatorWidget;
+use crate::config::{APP_ID, PROFILE};
+
+pub struct Window {
+    pub widget: libhandy::ApplicationWindow,
+    pub paginator: RefCell<Rc<PaginatorWidget>>,
+}
+
+impl Window {
+    pub fn new(app: &gtk::Application) -> Self {
+        let widget = libhandy::ApplicationWindow::new();
+        widget.set_application(Some(app));
+
+        let paginator = RefCell::new(PaginatorWidget::new());
+
+        let mut window_widget = Window { widget, paginator };
+
+        window_widget.init();
+        window_widget
+    }
+
+    pub fn start_tour(&self) {
+        self.paginator.borrow_mut().set_page(1);
+    }
+
+    pub fn reset_tour(&self) {
+        self.paginator.borrow_mut().set_page(0);
+    }
+
+    fn init(&mut self) {
+        self.widget.set_default_size(960, 720);
+        self.widget.set_icon_name(Some(APP_ID));
+
+        // Devel Profile
+        if PROFILE == "Devel" {
+            self.widget.get_style_context().add_class("devel");
+        }
+        self.paginator
+            .borrow_mut()
+            .add_page(WelcomePageWidget::new().widget.upcast::<gtk::Widget>());
+        self.paginator.borrow_mut().add_page(
+            ImagePageWidget::new(
+                "/org/gnome/Tour/overview.svg",
+                gettext("Get an Overview"),
+                gettext("Press Activities to see open windows and apps."),
+            )
+            .widget
+            .upcast::<gtk::Widget>(),
+        );
+
+        self.paginator.borrow_mut().add_page(
+            ImagePageWidget::new(
+                "/org/gnome/Tour/makeyourown.svg",
+                gettext("Make Apps Your Own"),
+                gettext("Arrange the app grid to your liking."),
+            )
+            .widget
+            .upcast::<gtk::Widget>(),
+        );
+
+        self.paginator.borrow_mut().add_page(
+            ImagePageWidget::new(
+                "/org/gnome/Tour/workspaces.svg",
+                gettext("Keep on Top with Workspaces"),
+                gettext("Easily organise windows with the new workspaces view."),
+            )
+            .widget
+            .upcast::<gtk::Widget>(),
+        );
+
+        self.paginator.borrow_mut().add_page(
+            ImagePageWidget::new(
+                "/org/gnome/Tour/blank.svg",
+                gettext("Up/Down for the Overview"),
+                gettext("On a touchpad, use three-finger vertical swipes. Try it!"),
+            )
+            .widget
+            .upcast::<gtk::Widget>(),
+        );
+
+        self.paginator.borrow_mut().add_page(
+            ImagePageWidget::new(
+                "/org/gnome/Tour/blank.svg",
+                gettext("Left/Right for Workspaces"),
+                gettext("On a touchpad, use three-finger horizontal swipes. Try it!"),
+            )
+            .widget
+            .upcast::<gtk::Widget>(),
+        );
+
+        let name = glib::get_os_info("NAME").unwrap_or_else(|| "GNOME".into());
+        let version = glib::get_os_info("VERSION_ID").unwrap_or_else(|| "".into());
+        let last_page = ImagePageWidget::new(
+            "/org/gnome/Tour/ready-to-go.svg",
+            // Translators: The following string is formatted as "We hope that you enjoy GNOME 40"
+            i18n_f(
+                "That's it! We hope that you enjoy {} {}.",
+                &[&name, &version],
+            ),
+            gettext("To get more advice and tips, see the Help app."),
+        );
+        last_page.widget.get_style_context().add_class("last-page");
+        self.paginator
+            .borrow_mut()
+            .add_page(last_page.widget.upcast::<gtk::Widget>());
+
+        self.widget.add(&self.paginator.borrow().widget);
+    }
+}
